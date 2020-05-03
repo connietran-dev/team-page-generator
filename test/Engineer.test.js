3a2d1b1c4e9d2d7b7a0a21c0f5e69836a318b7c6
Connie Tran <connietrandev@gmail.com>
2020-05-03 14:28:53 -0400

    Add additional tests to Engineer to ensure constructors and methods are inherited from Employee

--- test/Engineer.test.js
+++ test/Engineer.test.js
@@ -1,12 +1,35 @@
 const Engineer = require("../lib/Engineer");
 
+test("Can instantiate Engineer instance", () => {
+  const e = new Engineer();
+  expect(typeof(e)).toBe("object");
+});
+
+test("Can set name via super", () => {
+  const name = "Louise the Engineer";
+  const e = new Engineer(name);
+  expect(e.name).toBe(name);
+});
+
+test("Can set id via super", () => {
+  const testValue = 251;
+  const e = new Engineer("Carol", testValue);
+  expect(e.id).toBe(testValue);
+});
+
+test("Can get email via super", () => {
+  const testValue = "testeng@test.com";
+  const e = new Engineer("David", 122, testValue);
+  expect(e.getEmail()).toBe(testValue);
+});
+
 test("Can set GitHub account via constructor", () => {
   const testValue = "GitHubUser";
   const e = new Engineer("Foo", 1, "test@test.com", testValue);
   expect(e.github).toBe(testValue);
 });
 
-test("getRole() should return \"Engineer\"", () => {
+test("getRole() method from parent Employee class should return \"Engineer\"", () => {
   const testValue = "Engineer";
   const e = new Engineer("Foo", 1, "test@test.com", "GitHubUser");
   expect(e.getRole()).toBe(testValue);
