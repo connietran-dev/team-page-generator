434653733bf04fafda4d00ae3222a36ea58367fc
Connie Tran <connietrandev@gmail.com>
2020-05-03 13:58:58 -0400

    Add Engineer class

--- test/Engineer.test.js
+++ test/Engineer.test.js
@@ -1,6 +1,6 @@
 const Engineer = require("../lib/Engineer");
 
-test("Can set GitHUb account via constructor", () => {
+test("Can set GitHub account via constructor", () => {
   const testValue = "GitHubUser";
   const e = new Engineer("Foo", 1, "test@test.com", testValue);
   expect(e.github).toBe(testValue);
