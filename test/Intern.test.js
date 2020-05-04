d72c55fd54dd1600573754d587d283a0dd735eff
Connie Tran <connietrandev@gmail.com>
2020-05-04 15:27:56 -0400

    Add Intern class

--- test/Intern.test.js
+++ test/Intern.test.js
@@ -1,5 +1,28 @@
 const Intern = require("../lib/Intern");
 
+test("Can instantiate Intern instance", () => {
+  const e = new Intern();
+  expect(typeof(e)).toBe("object");
+});
+
+test("Can set name via super", () => {
+  const name = "Greg the Intern";
+  const e = new Intern(name);
+  expect(e.name).toBe(name);
+});
+
+test("Can set id via super", () => {
+  const testValue = 576;
+  const e = new Intern("Greg", testValue);
+  expect(e.id).toBe(testValue);
+});
+
+test("Can get email via super", () => {
+  const testValue = "testintern@test.com";
+  const e = new Intern("Greg", 687, testValue);
+  expect(e.getEmail()).toBe(testValue);
+});
+
 test("Can set school via constructor", () => {
   const testValue = "UCLA";
   const e = new Intern("Foo", 1, "test@test.com", testValue);
