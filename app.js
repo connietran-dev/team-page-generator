0be720a005cd165c2edea7fcc97f616cfa6e6057
Connie Tran <connietrandev@gmail.com>
2020-05-05 17:36:57 -0400

    Add Engineer and Intern prompts and objects

--- app.js
+++ app.js
@@ -1,24 +1,84 @@
+// External packages
 const inquirer = require('inquirer');
 const util = require('util');
 
-const questions = require('./questions');
+// Internal modules
 const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
 
+const questions = require('./questions');
+const render = require('./lib/htmlRenderer');
+
+// Main function
 async function init() {
     try {
-        
-        // Code to use Inquirer to gather information about development team members
-        // Based of responses, objects are created for each team member based off classes
 
-        const managerResponses = await inquirer.prompt(questions.manager);
-        
-        console.log("Thank you for your responses: ", managerResponses);
+        /* 
+        Code to use Inquirer to gather information about development team members. Based of responses, objects are created for each team member based off classes.
+        */
 
-        const newManager = new Manager(managerResponses.mgrName, managerResponses.mgrId,
-        managerResponses.mgrEmail,
-        managerResponses.mgrOffice);
+        // Collect information about required Manager role
+        let managerResponses = await inquirer.prompt(questions.manager);
 
-        console.log("We've create a new manager from your responses: ", newManager);
+        let newManager = new Manager
+            (managerResponses.mgrName,
+                managerResponses.mgrId,
+                managerResponses.mgrEmail,
+                managerResponses.mgrOffice);
+
+        const employees = [];
+
+        employees.push(newManager);
+
+        console.log("Thanks! We've added a new manager to the team: ", newManager);
+
+
+        // Next, ask if they'd like to create another team member
+        let createEmployee = await inquirer.prompt(questions.create);
+
+        let employeeRole;
+
+        switch (createEmployee.createEmp) {
+            case false:
+                console.log('Thank you for your input so far! Generating the HTML page next...')
+                return;
+            case true:
+                employeeRole = await inquirer.prompt(questions.employee);
+        };
+
+
+        // If yes they'd like to add another team member, ask Engineer or Intern questions
+        // Then create new object from class and add to employee array
+        switch (employeeRole.empRole) {
+            case 'Engineer':
+                let engResponses = await inquirer.prompt(questions.engineer);
+                let newEngineer = new Engineer
+                    (engResponses.engName,
+                        engResponses.engId,
+                        engResponses.engEmail,
+                        engResponses.engGithub);
+                employees.push(newEngineer);
+                console.log("Thanks! We've added a new engineer to the team: ", newEngineer);
+                break;
+            case 'Intern':
+                let internResponses = await inquirer.prompt(questions.intern);
+                let newIntern = new Intern
+                    (internResponses.internName,
+                        internResponses.internId,
+                        internResponses.internEmail,
+                        internResponses.internSchool);
+                employees.push(newIntern);
+                console.log("Thanks! We've added a new intern to the team: ", newIntern);
+        };
+
+
+
+        // After the user has input all employees desired, call the render function
+        // and pass an array containing all employee objects
+        // The render function will generate and return a block fo HTML including templated divs for each employee
+
+        // console.log(render(employees));
 
 
     } catch (error) {
--- questions.js
+++ questions.js
@@ -6,7 +6,7 @@ const managerQuestions = [
         default: 'Ann',
         validate: function (answer) {
             if (answer.length < 1) {
-                return console.log("A valid manager name is required.");
+                return console.log("A valid name is required.");
             }
             return true;
         }
@@ -30,7 +30,7 @@ const managerQuestions = [
         default: 'testmgr@test.com',
         validate: function (answer) {
             if (answer.length < 1) {
-                return console.log("A valid manager email address is required.");
+                return console.log("A valid email address is required.");
             }
             return true;
         }
@@ -42,13 +42,138 @@ const managerQuestions = [
         default: '423',
         validate: function (answer) {
             if (answer.length < 1) {
-                return console.log("A valid manager office number is required.");
+                return console.log("A valid office number is required.");
             }
             return true;
         }
     },
 ];
 
+
+const createEmployee = [
+    {
+        type: 'confirm',
+        message: "Next, would you like to add another team member to the team?",
+        name: 'createEmp'
+    }
+];
+
+const employeeType = [
+    {
+        type: 'list',
+        message: "Would you like to add an Engineer or Intern to the team?",
+        choices: ['Engineer', 'Intern'],
+        name: 'empRole'
+    }
+];
+
+
+const engineerQuestions = [
+    {
+        type: 'input',
+        message: "What is your software engineer's name?",
+        name: 'engName',
+        default: 'Louise',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("A valid name is required.");
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        message: "What is your engineer's employee ID?",
+        name: 'engId',
+        default: '296',
+        validate: function (answer) {
+            if (answer <= 0) {
+                return console.log("A valid employee ID is required.");
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        message: "What is your engineer's email address?",
+        name: 'engEmail',
+        default: 'testeng@test.com',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("A valid email address is required.");
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        message: "What is your engineer's GitHub? (No @ needed)",
+        name: 'engGithub',
+        default: 'connietran-dev',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("A valid GitHub is required.");
+            }
+            return true;
+        }
+    }
+];
+
+const internQuestions = [
+    {
+        type: 'input',
+        message: "What is your intern's name?",
+        name: 'internName',
+        default: 'Greg',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("A valid name is required.");
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        message: "What is your intern's employee ID?",
+        name: 'internId',
+        default: '685',
+        validate: function (answer) {
+            if (answer <= 0) {
+                return console.log("A valid employee ID is required.");
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        message: "What is your intern's email address?",
+        name: 'internEmail',
+        default: 'testintern@test.com',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("A valid email address is required.");
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        message: "What is the name of your intern's university?",
+        name: 'internSchool',
+        default: 'Georgia Institute of Technology',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("A valid school is required.");
+            }
+            return true;
+        }
+    }
+];
+
 module.exports = {
-    manager: managerQuestions
+    manager: managerQuestions,
+    create: createEmployee,
+    employee: employeeType,
+    engineer: engineerQuestions,
+    intern: internQuestions
 };
